d2f75c36c89c76dd16d2527bfc4e528570496aa2
Rogovenko Alexandra <workalexro@gmail.com>
2020-06-20 10:09:46 +0300

    Initial commit

---
+++ Organizel_0/Organizel/src/main/java/org/Organizel/Organizel/OrganizelApplication.java
@@ -0,0 +1,13 @@
+package org.Organizel.Organizel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizelApplication.class, args);
+	}
+
+}
---
+++ Organizel_0/Organizel/src/test/java/org/Organizel/Organizel/OrganizelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Organizel.Organizel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
